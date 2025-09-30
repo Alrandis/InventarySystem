5596ce5c16f1dd06a64f3b266eca4e59f4ba3a0e
Павлов Алексей <79513543+Alrandis@users.noreply.github.com>
2025-09-30 23:14:33 +0700

    feat(data): create base class ItemData

---
+++ Assets/Scripts/Inventory/Data/ItemData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "Scriptable Objects/ItemData")]
+public abstract class ItemData : ScriptableObject
+{
+    public string Name;
+    public string Description;
+    public Sprite Icon;
+    public GameObject PreviewPrefab;
+}
+
+public enum ItemType
+{
+    Weapon,
+    Armor,
+    Potion,
+    Key,
+    Quest
+}
