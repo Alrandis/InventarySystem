a4d08a913026d54c5b93b21f928b9f605edeae6b
Павлов Алексей <79513543+Alrandis@users.noreply.github.com>
2025-10-01 00:34:12 +0700

    feat(date): create interface IItemInstance
    - added interface implementation in WeaponInstance
    - added interface implementation in ArmorInstance

--- Assets/Scripts/Inventory/Data/ArmorInstance.cs
+++ Assets/Scripts/Inventory/Data/ArmorInstance.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 [System.Serializable]
-public class ArmorInstance
+public class ArmorInstance : IItemInstance
 {
     public ArmorItem ArmorType;
 
     public int Defence;
+
+    public ItemData ItemData => ArmorType;
 
     public ArmorInstance(ArmorItem armor, int defaulDefense)
     {
--- Assets/Scripts/Inventory/Data/WeaponInstance.cs
+++ Assets/Scripts/Inventory/Data/WeaponInstance.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 
 [System.Serializable]
-public class WeaponInstance 
+public class WeaponInstance : IItemInstance
 {
     public WeaponItem WeaponType;
 
     public int Damage;
     public float AttackSpeed;
+
+    public ItemData ItemData => WeaponType;
 
     public WeaponInstance(WeaponItem weapon, int startingDamage, float startingAttackSpeed) 
     {
